49b09508c165da7af92b8db24bf50a40a3a9e031
egrc3uh <sumit.dubey@mckesson.com>
2021-06-03 13:44:32 +0530

    initial

---
+++ src/test/java/epam/demo/libraryservice/LibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package epam.demo.libraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
